d103637e260310944648f088fd819a2300ee88c1
PabloBP31 <Pablopelayo31@gmail.com>
2023-02-21 21:39:48 +0100

    Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xbbE4039209851Ce94285Ee500ec744FdDba2034D";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
